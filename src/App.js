85bed5533d787864b76f5e872fe95d71d5a7115b
Cameron Partee <cp3082@nyu.edu>
2021-07-11 18:48:30 -0400

    project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Now that ive got you down to a tee shirt...</h1>
+    </div>
+  );
+}
+
+export default App;

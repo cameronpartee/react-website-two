d6737c7ef1fbe4854907eaba6c2b363cacef70f8
Cameron Partee <cp3082@nyu.edu>
2021-07-11 20:56:36 -0400

    Add card component and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home'
+import Services from './components/pages/Service';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
@@ -8,7 +12,10 @@ function App() {
     <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact />
+          <Route path='/' exact component={Home}/>
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
         </Switch>
     </Router>
     </>
